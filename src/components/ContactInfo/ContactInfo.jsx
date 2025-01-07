28503b60e126c9184be660b88405f6da242a8c63
Laurencehumble <148223895+Laurencehumble@users.noreply.github.com>
2025-01-07 16:28:26 +0800

    Update ContactInfo.jsx

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -5,9 +5,9 @@ const ContactInfo = () => {
   return (
     <section id="contact" className="py-16">
       <div className="container mx-auto px-6 md:px-12">
-      <div className="text-center mb-12">
+        <div className="text-center mb-12">
           <h2
-            className="text-4xl sm:text-5xl font-bold font-patua_one mb-4 text-gray-900"
+            className="text-3xl sm:text-4xl lg:text-5xl font-bold font-patua_one mb-4 text-gray-900"
             data-aos="fade-up"
             data-aos-duration="500"
             data-aos-delay="300"
@@ -20,38 +20,41 @@ const ContactInfo = () => {
             data-aos-duration="500"
             data-aos-delay="400"
           >
-           Get in touch with me for job opportunities or to discuss how I can contribute to your team. I’m eager to bring my skills and expertise to your projects.
+            Get in touch with me for job opportunities or to discuss how I can contribute to your team. I’m eager to bring my skills and expertise to your projects.
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {/* Phone */}
-          <div className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
-           data-aos="fade-up"
-           data-aos-duration="500"
-           data-aos-delay="400"
+          <div
+            className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
+            data-aos="fade-up"
+            data-aos-duration="500"
+            data-aos-delay="400"
           >
             <IoCall className="text-4xl text-[#12867F] mb-4" />
-            <h3 className="text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Phone</h3>
+            <h3 className="text-lg sm:text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Phone</h3>
             <a href="tel:09217422647"><p className="text-[#4E635C] font-patua_one">09217422647</p></a>
           </div>
           {/* Email */}
-          <div className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
-           data-aos="fade-up"
-           data-aos-duration="500"
-           data-aos-delay="400"
+          <div
+            className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
+            data-aos="fade-up"
+            data-aos-duration="500"
+            data-aos-delay="400"
           >
             <IoMail className="text-4xl text-[#12867F] mb-4" />
-            <h3 className="text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Email</h3>
+            <h3 className="text-lg sm:text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Email</h3>
             <a href="mailto:rheylaurencebuday@gmail.com"><p className="text-[#4E635C] font-patua_one">rheylaurencebuday@gmail.com</p></a>
           </div>
           {/* Address */}
-          <div className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
-           data-aos="fade-up"
-           data-aos-duration="500"
-           data-aos-delay="400"
+          <div
+            className="flex flex-col items-center text-center bg-white shadow-lg rounded-lg p-6"
+            data-aos="fade-up"
+            data-aos-duration="500"
+            data-aos-delay="400"
           >
             <IoLocationSharp className="text-4xl text-[#12867F] mb-4" />
-            <h3 className="text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Address</h3>
+            <h3 className="text-lg sm:text-xl font-semibold text-[#4E635C] mb-2 font-patua_one">Address</h3>
             <p className="text-[#4E635C] font-patua_one">1 Janet St Ext, Tandang Sora Banlat, Quezon City</p>
           </div>
         </div>
